a2194ab910a6b1c23aa9d8561330495478129c5f
G M <propbono@gmail.com>
2023-04-22 15:36:06 -0400

    feat: change darkMode to class and add some basic colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00c6bd",
+        secondary: "#111827",
+        tertiary: "#f5f5f5",
+        "shade-100": "#c4cbe0",
+        "shade-300": "#9399ad",
+        "shade-500": "#646a7d",
+        "shade-800": "#393f50",
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;
