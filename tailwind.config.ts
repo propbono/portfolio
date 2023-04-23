26af2988bc42d0046f543f5dc62ab475620f5542
G M <propbono@gmail.com>
2023-04-22 22:28:53 -0400

    feat: add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#00c6bd",
-        secondary: "#111827",
-        tertiary: "#f5f5f5",
         "shade-100": "#c4cbe0",
         "shade-300": "#9399ad",
         "shade-500": "#646a7d",
         "shade-800": "#393f50",
+        "primary-accent": "#00c6bd",
+        "primary-dark": "#111827",
+        "primary-light": "#f5f5f5",
+        "secondary-accent": "#00425b",
+        "secondary-dark": "#393f50",
+        "secondary-light": "#c4cbe0",
+        "tertiary-accent": "#007380",
       },
+    },
+    fontFamily: {
+      logo: ["Righteous", "cursive"],
+      body: ["Ubuntu", "sans-serif"],
     },
   },
   darkMode: "class",
