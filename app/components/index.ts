92c4de3b477baf144a4c44279c3b4b1346edbc14
G M <propbono@gmail.com>
2023-07-11 19:11:01 -0400

    fix: remove logo from index

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./header/header";
-export * from "./logo/logo";
 export * from "./custom-nav-link/custom-nav-link";
 export * from "./footer/footer";
 export * from "./container/container";
