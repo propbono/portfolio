8304deba9849b8c390850965d90cfde174ff8afb
G M <propbono@gmail.com>
2023-07-16 15:11:15 -0400

    fix: update meta title in index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { V2_MetaFunction } from "@remix-run/node";
 import { AboutSection, HeroSection, TechStack } from "~/components";
 
 export const meta: V2_MetaFunction = () => [
-  { title: "Propbono - Portfolio Website" },
+  { title: "Portfolio Website by propbono" },
 ];
 
 export default function Index() {
